a3ca9b800490932b78e935d15db86740446b5d7d
ChrisR-BEIS <64835682+ChrisR-BEIS@users.noreply.github.com>
2020-05-29 13:33:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 //--------------------------------------------------------------------
 // Variables
-variable "resource_group_name" {}
+variable "compute_resource_group_name" {}
 variable "compute_vm_hostname" {}
+variable "network_resource_group_name" {}
 
 //--------------------------------------------------------------------
 // Modules
-
 module "compute" {
   source  = "tfe.ddat.team/DDAT/compute/azurerm"
   version = "3.2.0"
 
   nb_instances = 1
   nb_public_ip = 0
-  resource_group_name = var.resource_group_name
-  vm_hostname = var.compute_vm_hostname
+  resource_group_name = "${var.compute_resource_group_name}"
+  vm_hostname = "${var.compute_vm_hostname}"
   vm_os_simple = "UbuntuServer"
   vnet_subnet_id = "${module.network.vnet_subnets[0]}"
 }
@@ -22,5 +22,5 @@ module "network" {
   source  = "tfe.ddat.team/DDAT/network/azurerm"
   version = "3.1.1"
 
-  resource_group_name = var.resource_group_name
+  resource_group_name = "${var.network_resource_group_name}"
 }
