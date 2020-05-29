c85f75e5cee22c8132ee43781bf42676df6ade20
ChrisR-BEIS <64835682+ChrisR-BEIS@users.noreply.github.com>
2020-05-29 13:09:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,23 +6,13 @@ variable "compute_vm_hostname" {}
 //--------------------------------------------------------------------
 // Modules
 
-provider "azurerm"{
-  version = "~>2.0"
-  features {}
-}
-
-resource "azure_resource_group" "rg01" {
-  name = var.resource_group_name
-  location = "UKSouth"
-}
-
 module "compute" {
   source  = "tfe.ddat.team/DDAT/compute/azurerm"
   version = "3.2.0"
 
   nb_instances = 1
   nb_public_ip = 0
-  resource_group_name = azure_resource_group.rg01.name
+  resource_group_name = var.resource_group_name
   vm_hostname = var.compute_vm_hostname
   vm_os_simple = "UbuntuServer"
   vnet_subnet_id = "${module.network.vnet_subnets[0]}"
@@ -32,5 +22,5 @@ module "network" {
   source  = "tfe.ddat.team/DDAT/network/azurerm"
   version = "3.1.1"
 
-  resource_group_name = azure_resource_group.rg01.name
+  resource_group_name = var.resource_group_name
 }
