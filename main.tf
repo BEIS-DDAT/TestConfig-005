02a447b4842a3b226bd819524a6c2a949bf44e04
ChrisR-BEIS <64835682+ChrisR-BEIS@users.noreply.github.com>
2020-05-29 12:59:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,27 @@
 //--------------------------------------------------------------------
 // Variables
-variable "compute_public_ip_dns" {}
-variable "compute_resource_group_name" {}
+variable "resource_group_name" {}
 variable "compute_vm_hostname" {}
-variable "network_resource_group_name" {}
 
 //--------------------------------------------------------------------
 // Modules
+
+provider "azurerm"{
+  features {}
+}
+
+resource "azure_resource_group" "rg01" {
+  name = "${var.resource_group_name}
+  location = "UKSouth"
+}
+
 module "compute" {
   source  = "tfe.ddat.team/DDAT/compute/azurerm"
-  version = "2.0.0"
+  version = "3.2.0"
 
-  location = "uksouth"
   nb_instances = 1
   nb_public_ip = 0
-  public_ip_dns = "${var.compute_public_ip_dns}"
-  resource_group_name = "${var.compute_resource_group_name}"
+  resource_group_name = azure_resource_group.rg01.name
   vm_hostname = "${var.compute_vm_hostname}"
   vm_os_simple = "UbuntuServer"
   vnet_subnet_id = "${module.network.vnet_subnets[0]}"
@@ -23,8 +29,7 @@ module "compute" {
 
 module "network" {
   source  = "tfe.ddat.team/DDAT/network/azurerm"
-  version = "2.0.0"
+  version = "3.1.1"
 
-  location = "uksouth"
-  resource_group_name = "${var.network_resource_group_name}"
+  resource_group_name = azure_resource_group.rg01.name
 }
